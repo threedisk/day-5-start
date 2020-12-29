b3675324a93d28093590d364dadd6d6f17e0331c
threedisk <threedisk@gmail.com>
2020-12-29 11:18:27 +0000

    For Loops in Python

---
+++ main.py
@@ -0,0 +1,9 @@
+#For Loops for Python List
+fruits = ['Apple', 'Blueberry', 'Grape', 'Banana']
+#print (fruits)
+line = '=================='
+print (line)
+print ('For Loops for Python List')
+print (line)
+for fruit in fruits:
+  print(fruit)
